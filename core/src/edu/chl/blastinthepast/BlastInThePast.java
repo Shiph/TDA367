eb06440f1e353fcbb908b349db449b51e30e5768
Shiph <Shif@dhcp-181126.eduroam.chalmers.se>
2015-04-15 14:53:13 +0200

    Testing to push libGDX setup

--- core/src/edu/chl/blastinthepast/BlastInThePast.java
+++ core/src/edu/chl/blastinthepast/BlastInThePast.java
@@ -1,4 +1,4 @@
-package com.mygdx.game;
+package edu.chl.blastinthepast;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class BlastInThePast extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
