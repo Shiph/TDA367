8628fa74c030527222527964a5bc0042e91aeaff
ceerik <asdert458@gmail.com>
2015-04-23 14:22:56 +0200

    New Interface Collidable

--- core/src/edu/chl/blastinthepast/model/Collidable.java
+++ core/src/edu/chl/blastinthepast/model/Collidable.java
@@ -1,7 +1,12 @@
 package edu.chl.blastinthepast.model;
 
+import com.badlogic.gdx.maps.Map;
+import com.badlogic.gdx.math.Rectangle;
+
 /**
  * Created by qwerty458 on 4/23/15.
  */
 public interface Collidable {
+    Rectangle rectangle = new Rectangle();
+    Rectangle getRectangle();
 }
