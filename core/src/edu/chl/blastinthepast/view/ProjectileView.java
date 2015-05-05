c7395bc8b3b70e16e2c3ec6e4c1e7b56b7739286
ceerik <asdert458@gmail.com>
2015-05-05 14:57:21 +0200

    collision detection wip

--- core/src/edu/chl/blastinthepast/view/ProjectileView.java
+++ core/src/edu/chl/blastinthepast/view/ProjectileView.java
@@ -5,16 +5,17 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 import edu.chl.blastinthepast.model.Projectile;
 import com.badlogic.gdx.audio.Sound;
 
 /**
  * Created by jonas on 2015-04-23.
  */
-public class ProjectileView {
+public class ProjectileView implements Collidable {
     private Texture texture;
     private Sprite sprite;
-    private Rectangle rectangle;
+    private Array<Rectangle> rectangle;
     private Projectile projectile;
     //private Sound fireSound = Gdx.audio.newSound(Gdx.files.internal("wow.mp3"));
 
@@ -22,12 +23,15 @@ public class ProjectileView {
         projectile=newProjectile;
         texture = new Texture(Gdx.files.local("triforce.png"));
         sprite = new Sprite(texture);
-        rectangle = new Rectangle();
-        rectangle.height = 64;
-        rectangle.width = 64;
-        sprite.setX(rectangle.x);
-        sprite.setY(rectangle.y);
-        //fireSound.play();
+        rectangle = new Array(true,1,Rectangle.class);
+        rectangle.size = 1;
+        rectangle.set(0, new Rectangle());
+        rectangle.get(0).x = 800/2 - 64/2;
+        rectangle.get(0).y = 480/2 - 64/2;
+        rectangle.get(0).height = 64;
+        rectangle.get(0).width = 64;
+        sprite.setX(rectangle.get(0).x);
+        sprite.setY(rectangle.get(0).y);
     }
     /**
      *
@@ -47,14 +51,14 @@ public class ProjectileView {
 
     private void updatePosition(){
         sprite.setPosition(projectile.getPosition().getX(), projectile.getPosition().getY());
-        rectangle.setPosition(projectile.getPosition().getX(), projectile.getPosition().getY());
+        rectangle.get(0).setPosition(projectile.getPosition().getX(), projectile.getPosition().getY());
     }
 
     /**
      *
      * @return the rectangle of the projectile.
      */
-    public Rectangle getRectangle() {
+    public Array<Rectangle> getRectangles() {
         return rectangle;
     }
 
@@ -71,4 +75,8 @@ public class ProjectileView {
         texture.dispose();
     }
 
+    @Override
+    public void setRectangles(Array<Rectangle> rectangles) {
+
+    }
 }
