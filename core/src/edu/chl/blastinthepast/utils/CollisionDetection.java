c15997f681c892f82893fa5c7aab88f63e19073a
ceerik <asdert458@gmail.com>
2015-05-05 16:40:08 +0200

    hopefully fixed some shit

--- core/src/edu/chl/blastinthepast/utils/CollisionDetection.java
+++ core/src/edu/chl/blastinthepast/utils/CollisionDetection.java
@@ -3,14 +3,23 @@ package edu.chl.blastinthepast.utils;
 
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
-import edu.chl.blastinthepast.view.Collidable;
+import edu.chl.blastinthepast.model.Enemy;
+import edu.chl.blastinthepast.view.*;
+
+import java.util.ArrayList;
 
 /**
  * Created by qwerty458 on 4/23/15.
  */
 public final class CollisionDetection {
 
-    public boolean collisionDetector(Collidable c1, Collidable c2) {
+    public void update (ArrayList<EnemyView> enemies, PlayerView player, ArrayList<ProjectileView> projectiles, ChestView chest, CollisionView collisions) {
+        for(EnemyView e : enemies) {
+        }
+    }
+
+    public static boolean collisionDetector(Collidable c1, Collidable c2) {
+
         Array<Rectangle> r1 = new Array<Rectangle>();
         Array<Rectangle> r2 = new Array<Rectangle>();
 
@@ -23,6 +32,63 @@ public final class CollisionDetection {
                 if (r1.get(i).overlaps(r2.get(k))) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+
+    private static final class Enemies {
+
+        private boolean enemiesVSChest(ArrayList<EnemyView> enemies, ChestView chest) {
+            for (EnemyView e : enemies) {
+                if (collisionDetector(e, chest)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private boolean enemiesVSCollisions(ArrayList<EnemyView> enemies, CollisionView collisions) {
+            for (EnemyView e : enemies) {
+                if (collisionDetector(e, collisions)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    private static final class Player {
+
+        private boolean playerVSChest (PlayerView player, ChestView chest) {
+            return collisionDetector(player, chest);
+        }
+
+        private boolean playerVSCollision (PlayerView player, CollisionView collisions) {
+            return collisionDetector(player, collisions);
+        }
+    }
+
+    private static final class Projectiles {
+
+        private boolean projectilesVSPlayer (ArrayList<ProjectileView> projectiles, PlayerView player) {
+            for (ProjectileView p : projectiles) {
+                if (collisionDetector(p, player)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private boolean projectilesVSPlayer (ArrayList<ProjectileView> projectiles, Array<EnemyView> enemies ) {
+            return true;
+        }
+    }
+
+    private boolean enemiesVSPlayer (ArrayList < EnemyView > enemies, PlayerView player) {
+        for (EnemyView e : enemies) {
+            if (collisionDetector(e, player)) {
+                return true;
             }
         }
         return false;
--- core/src/edu/chl/blastinthepast/view/ProjectileView.java
+++ core/src/edu/chl/blastinthepast/view/ProjectileView.java
@@ -5,16 +5,19 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 import edu.chl.blastinthepast.model.Projectile;
 import com.badlogic.gdx.audio.Sound;
+
+import java.util.ArrayList;
 
 /**
  * Created by jonas on 2015-04-23.
  */
-public class ProjectileView {
+public class ProjectileView implements Collidable {
     private Texture texture;
     private Sprite sprite;
-    private Rectangle rectangle;
+    private ArrayList<Rectangle> rectangle;
     private Projectile projectile;
     //private Sound fireSound = Gdx.audio.newSound(Gdx.files.internal("wow.mp3"));
 
@@ -22,12 +25,24 @@ public class ProjectileView {
         projectile=newProjectile;
         texture = new Texture(Gdx.files.local("triforce.png"));
         sprite = new Sprite(texture);
-        rectangle = new Rectangle();
-        rectangle.height = 64;
-        rectangle.width = 64;
-        sprite.setX(rectangle.x);
-        sprite.setY(rectangle.y);
+<<<<<<< HEAD
+        rectangle = new ArrayList<Rectangle>();
+        rectangle.add(new Rectangle());
+        rectangle.get(0).height = 64;
+        rectangle.get(0).width = 64;
+        sprite.setX(rectangle.get(0).x);
+        sprite.setY(rectangle.get(0).y);
         //fireSound.play();
+=======
+        rectangle = new ArrayList<Rectangle>;
+        rectangle.add(0, new Rectangle());
+        rectangle.get(0).x = 800/2 - 64/2;
+        rectangle.get(0).y = 480/2 - 64/2;
+        rectangle.get(0).height = 64;
+        rectangle.get(0).width = 64;
+        sprite.setX(rectangle.get(0).x);
+        sprite.setY(rectangle.get(0).y);
+>>>>>>> collision detection wip
     }
     /**
      *
@@ -47,14 +62,14 @@ public class ProjectileView {
 
     private void updatePosition(){
         sprite.setPosition(projectile.getPosition().getX(), projectile.getPosition().getY());
-        rectangle.setPosition(projectile.getPosition().getX(), projectile.getPosition().getY());
+        rectangle.get(0).setPosition(projectile.getPosition().getX(), projectile.getPosition().getY());
     }
 
     /**
      *
      * @return the rectangle of the projectile.
      */
-    public Rectangle getRectangle() {
+    public Array<Rectangle> getRectangles() {
         return rectangle;
     }
 
@@ -66,9 +81,16 @@ public class ProjectileView {
         batch.end();
     }
 
+<<<<<<< HEAD
     public void dispose(){
         texture.dispose();
         //fireSound.dispose();
     }
 
+=======
+    @Override
+    public void setRectangles(Array<Rectangle> rectangles) {
+
+    }
+>>>>>>> collision detection wip
 }
